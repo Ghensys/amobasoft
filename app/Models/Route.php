7b38dd476e0b388003736e6cc588c6ea2730166e
Ghensys Valero <ghensysv@gmail.com>
2022-01-29 19:10:40 -0400

    Correcion de nombres para match con la tabla routes_data

--- app/Models/Route.php
+++ app/Models/Route.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Route extends Model
 {
     use HasFactory;
+
+    public function routeData()
+    {
+        return $this->hasMany('App\Models\RoutesData');
+    }
 }
