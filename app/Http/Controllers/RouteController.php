3ef6dac8f7725735db2454d6f4aaa2e58691a8e6
Ghensys Valero <ghensysv@gmail.com>
2022-01-29 23:50:31 -0400

    Servicio capacidad por ruta segun id

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -17,6 +17,26 @@ class RouteController extends Controller
     {
         $route = Route::find($id);
 
-        return $route->routeData;
+        $data['id'] = $route->id;
+        $data['invitation_code'] = $route->invitation_code;
+        $data['title'] = $route->title;
+        $data['start_timestamp'] = $route->start_timestamp;
+        $data['end_timestamp'] = $route->end_timestamp;
+
+        $data['route_data'] = $route->routeData;
+        
+        // //Planes de usuario asociados al usuario
+        // foreach ($route->routeData as $key_userkey_route_data_plan => $value_route_data) {
+        //     $data['user_plan'][$key_route_data] = [
+        //         '' => $value_route_data->,
+        //         '' => $value_route_data->,
+        //         '' => $value_route_data->,
+        //         '' => $value_route_data->,
+        //         '' => $value_route_data->,
+        //         '' => $value_route_data->
+        //     ];
+
+
+        return $route;
     }
 }
